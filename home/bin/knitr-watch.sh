aae3be031d11cd1d218979ef611eda0a54758902
vonpupp <vonpupp@gmail.com>
2015-02-23 22:10:20 -0300

    Added knitr-watch.sh

---
+++ home/bin/knitr-watch.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+FILE="${1}"
+
+if [[ $# != 1 ]]; then
+    echo "Usage: knitr <file>"
+    exit
+fi
+
+if [[ ! -f "${FILE}" ]]; then
+    echo "Uh oh."
+    echo "It looks like the file you provided me doesn't exist: ${FILE}"
+    exit
+fi
+
+hash kicker 2> /dev/null || {
+    echo "Oh dear. I require Kicker to work"
+    echo "Install it with:"
+    echo "    $ gem install kicker"
+    exit
+}
+
+KNITR="echo \"Rerunning Knitr...\"; Rscript -e \"library(knitr); knit('./${FILE}')\""
+
+echo "Watching ${FILE}..."
+#watchmedo shell-command \
+#    --patterns="${FILE}*" \
+#    --command="$KNITR" \
+    .
+kicker -e "${KNITR}" ${FILE}
