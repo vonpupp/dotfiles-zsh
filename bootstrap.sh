5fed013ab12c153876acf3b40a717335a357bd37
vonpupp <vonpupp@gmail.com>
2015-06-28 21:02:28 -0300

    Simiplifying things out

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 DIRNAME=`dirname $0`
 
-git clone https://github.com/vonpupp/bootstrap.castle $DIRNAME/setup
-$DIRNAME/setup/do-bootstrap.sh
-$DIRNAME/10-install.sh
+source $DIRNAME/.vars
+source ~/.homesick/repos/homeshick/homeshick.sh
+homeshick link $REPO_NAME
+
+#git clone https://github.com/vonpupp/bootstrap.castle $DIRNAME/setup
+#$DIRNAME/setup/do-bootstrap.sh
+#$DIRNAME/10-install.sh
 $DIRNAME/20-config.sh
 $DIRNAME/30-config-tools.sh
