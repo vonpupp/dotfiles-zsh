1cbb8ae0b86734016b35dcb4bcd7adc27be5765a
vonpupp <vonpupp@gmail.com>
2015-06-30 00:06:24 -0300

    Moved bin folder to dotfiles-apps

--- home/bin/bandwidth-test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-wget -O /dev/null http://speedtest.wdc01.softlayer.com/downloads/test10.zip
--- home/bin/diatheke.nlt-se.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-REGEX="s/.*[0-9]\{1,\}:[0-9]\{1,\}: \{0,\}//"
-diatheke -b nlt-se $@ | head -n -2 | sed "$REGEX" | tr '\n' ' '
