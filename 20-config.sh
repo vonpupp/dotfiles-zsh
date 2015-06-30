196e484267e3d9f83cd69ae7d09e354d1e4d48e6
vonpupp <vonpupp@gmail.com>
2015-06-29 22:33:33 -0300

    Simplifying dotfiles

--- 20-config.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-chsh -s $(which zsh)
-
-# oh-my-zsh
-git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-mv ~/.oh-my-zsh/plugins/taskwarrior/taskwarrior.plugin.zsh ~/.oh-my-zsh/plugins/taskwarrior/taskwarrior.plugin.old.zsh
-#ln -s ~/repos/dotfiles/.oh-my-zsh.custom/plugins/taskwarrior/taskwarrior.plugin.zsh ~/.oh-my-zsh/plugins/taskwarrior/taskwarrior.plugin.zsh
-#ln -s ~/repos/dotfiles/home/.oh-my-zsh.custom/themes/vonpupp.zsh-theme ~/.oh-my-zsh/themes/vonpupp.zsh-theme
--- bootstrap.sh
+++ bootstrap.sh
@@ -5,8 +5,18 @@ source $DIRNAME/.vars
 source ~/.homesick/repos/homeshick/homeshick.sh
 homeshick link $REPO_NAME
 
-#git clone https://github.com/vonpupp/bootstrap.castle $DIRNAME/setup
-#$DIRNAME/setup/do-bootstrap.sh
-#$DIRNAME/10-install.sh
-$DIRNAME/20-config.sh
-$DIRNAME/30-config-tools.sh
+# FZF
+git clone https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+# BASE16
+git clone https://github.com/chriskempson/base16-shell.git
+~/.config/base16-shell
+
+# OH-MY-ZSH
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+mv ~/.oh-my-zsh/plugins/taskwarrior/taskwarrior.plugin.zsh ~/.oh-my-zsh/plugins/taskwarrior/taskwarrior.plugin.old.zsh
+#ln -s ~/repos/dotfiles/.oh-my-zsh.custom/plugins/taskwarrior/taskwarrior.plugin.zsh ~/.oh-my-zsh/plugins/taskwarrior/taskwarrior.plugin.zsh
+#ln -s ~/repos/dotfiles/home/.oh-my-zsh.custom/themes/vonpupp.zsh-theme ~/.oh-my-zsh/themes/vonpupp.zsh-theme
+
+chsh -s $(which zsh)
