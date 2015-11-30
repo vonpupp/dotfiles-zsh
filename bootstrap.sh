e5f13855c48a0ee65af80d4b69eed209d051cc56
vonpupp <vonpupp@gmail.com>
2015-11-30 00:30:21 -0200

    Fix base-shell typo

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,8 +10,7 @@ git clone https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
 # BASE16
-git clone https://github.com/chriskempson/base16-shell.git
-~/.config/base16-shell
+git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
 
 # OH-MY-ZSH
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
