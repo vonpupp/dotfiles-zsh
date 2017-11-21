a787d07ec24e73466e9a8ff9a1f696b66f4e7ede
Albert De La Fuente Vigliotti <albert@haevas.com>
2017-11-21 18:10:56 -0200

    Update zsh

--- home/.zsh/keychain.sh
+++
@@ -1,5 +0,0 @@
-### START-Keychain ###
-# Let  re-use ssh-agent and/or gpg-agent between logins
-[ -f /usr/bin/keychain ] && eval `keychain --eval --agents ssh id_rsa`
-#source $HOME/.keychain/`hostname`-sh
-### End-Keychain ###
